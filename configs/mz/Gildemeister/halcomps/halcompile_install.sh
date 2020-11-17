7da14e6c9bdf7b1c4e85d1cca5d4fbe99c2ca3e9
MZot <zotler@gmail.com>
2020-11-17 21:37:02 +0100

    hitachi in gildemeister conf iz stroja

---
+++ configs/mz/Gildemeister/halcomps/halcompile_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+
+xfce4-terminal -x bash -c  'echo "Compiling and installing:";. \
+                            /home/cnc/LinuxCNC/linuxcnc/scripts/rip-environment ; \
+                            find ./ -name "*.comp" -exec sh -c "echo ; 
+                                                                echo -n {} ; 
+                                                                echo ; 
+                                                                halcompile --install {}" \; ; 
+                            echo ; \
+                            echo "done, check output for errors!" ; \
+                            read ; '
